c95c50539e779fdbf1f2192d5dc8d4c17434fc86
sloanlacey <sloanlacey89@gmail.com>
2021-03-30 15:34:41 -0700

    Create basic jumbotron header with input field for searching employee database.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Employee Directory</h1>
+    <>
+      <div className="jumbotron">
+        <h1 className="display-4">Employee Directory</h1>
+        <p className="lead">This is an employee database.</p>
+        <hr className="my-4"></hr>
+        <p>Search for employees by name:</p>
+          <form>
+            <label>Employee name:</label>
+            <input type="text"></input>
+          </form>
+        <p className="lead">
+        </p>
     </div>
+    </>
   );
 }
 
