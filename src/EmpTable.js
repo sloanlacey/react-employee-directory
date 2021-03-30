ac83777bf6f695270ba752ecac5f53a23b872b38
sloanlacey <sloanlacey89@gmail.com>
2021-03-30 16:50:33 -0700

    Add data cells to table with keys from API to render specific data points.

--- src/EmpTable.js
+++ src/EmpTable.js
@@ -13,14 +13,28 @@ const EmpTable = ({ employees }) => {
                         <th scope='col'>Last Name</th>
                         <th scope='col'>Email</th>
                         <th scope='col'>Birthday</th>
-                        <th scope='col'>Address</th>
+                        <th scope='col'>City</th>
+                        <th scope='col'>State</th>
                         <th scope='col'>Phone Number</th>
                         <th scope='col'>Password</th>
+                        <th scope='col'>Photo</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-
+                        <td>{title}</td>
+                        <td>{first}</td>
+                        <td>{last}</td>
+                        <td>{email}</td>
+                        <td>{dob}</td>
+                        <td>{city}</td>
+                        <td>{state}</td>
+                        <td>{cell}</td>
+                        <td>{password}</td>
+                        <td>
+                            <img src={thumbnail} />
+                        </td>
+                        <td></td>
                     </tr>
                 </tbody>
             </table>
